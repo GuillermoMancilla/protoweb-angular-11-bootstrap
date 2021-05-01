4741bdc3fd7671c930aeb7c1c6ce49d1cdd01c3b
GuillermoMancilla <g.andres.mancilla@gmail.com>
2021-04-30 21:01:25 -0400

    ver 1.0 protype web angular 11/bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    ListaComponent,
+    TablaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
